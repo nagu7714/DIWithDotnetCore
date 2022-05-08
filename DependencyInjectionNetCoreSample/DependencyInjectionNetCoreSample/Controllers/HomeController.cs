1c1a458bc22126e89d3d0a57256881d8690371c3
Nagaraj Natarajan <nagaraj.natarajan@boeing.com>
2022-05-08 10:35:08 +0530

    .net core dependency Injection constructor & action method

---
+++ DependencyInjectionNetCoreSample/DependencyInjectionNetCoreSample/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using DependencyInjectionNetCoreSample.Models;
+using DependencyInjectionNetCoreSample.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionNetCoreSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+      
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            //Constructor injection
+            _logger = logger;
+          
+        }
+
+        public IActionResult Index([FromServices] IProductService _productService) // action method injection
+        {
+            var products = _productService.GetProducts();
+            return View(products);
+         
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DependencyInjectionNetCoreSample/DependencyInjectionNetCoreSample/Service/IProduct.cs
@@ -0,0 +1,29 @@
+﻿using DependencyInjectionNetCoreSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionNetCoreSample.Service
+{
+   public interface IProductService
+    {
+
+        List<Product> GetProducts();
+    }
+
+    
+
+public class AmazonProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        {
+            return new List<Product>()
+        {
+            new Product() { Id = 1001, Name = "Apple AirPods Pro", Price = 249.00m },
+            new Product() { Id = 1002, Name = "Sony Noise Cancelling Headphones", Price = 199.00m },
+            new Product() { Id = 1003, Name = "Acer Aspire 5 Slim Laptop", Price = 346.00m }
+        };
+        }
+    }
+}
